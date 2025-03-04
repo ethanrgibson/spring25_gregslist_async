ec9544ed20a97fd22b2072b385f8dd639aa254e5
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-04 14:32:03 -0700

    service calling api, and putting it into app state

--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -1,8 +1,17 @@
+import { AppState } from "../AppState.js";
+import { House } from "../models/House.js";
+import { api } from "../utils/Axios.js";
+
 class HousesService {
 
 
-  getHouses() {
-    console.log('service hooked up');
+  async getHouses() {
+
+    const response = await api.get('api/houses')
+    console.log('house data', response);
+    const house = response.data.map(pojo => new House(pojo))
+    console.log('formatted house data', house);
+    AppState.houses = house
 
   }
 
