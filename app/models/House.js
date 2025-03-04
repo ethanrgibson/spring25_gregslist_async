f4f9156cdd2a4be2f79bea99801ea68da6489fee
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-04 15:07:36 -0700

    house cards styled

--- app/models/House.js
+++ app/models/House.js
@@ -19,21 +19,23 @@ export class House {
   get houseCard() {
 
     return `
-    <div class="row bg-light shadow car-border mb-2" style="border-color: coral;">
+    <div class="row bg-light shadow car-border mb-3" style="border-color: coral;">
     <div class="col-md-7">
       <div class="d-flex justify-content-between p-2">
         <div>
           <p class="fs-2"> ${this.year} </p>
-          <p>${this.price.toLocaleString()}</p>
-          <p>${this.bedrooms} <span>${this.levels}</span></p>
-          <p>${this.bathrooms}</p>
+          <p>${this.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+          <p>${this.bedrooms} Bedrooms</p>
+          <p>${this.levels} Levels</p>
+          <p>${this.bathrooms} Bathrooms</p>
+          <p class ="fs-3"> Description:</p> 
           <p>${this.description}</p>
         </div>
       </div>
     </div>
     <div class="col-md-5 p-0">
       <img src="${this.imgUrl}" alt="${this.year} ${this.price}"
-        class="house-form-img img-fluid">
+        class="house-form-img">
     </div>
     </div>
 
