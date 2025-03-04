d15af33dc56b6324d8028f0acf7badc7b004c01f
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-04 13:59:11 -0700

    model set up

---
+++ app/models/House.js
@@ -0,0 +1,22 @@
+export class House {
+
+  constructor(data) {
+
+    this.id = this.id
+    this.bedrooms = data.bedrooms
+    this.bathrooms = data.bedrooms
+    this.levels = data.levels
+    this.imgUrl = data.imgUrl
+    this.year = this.year
+    this.price = this.price
+    this.description = this.description
+    this.creatorId = this.creatorId
+    this.creator = data.creator
+
+
+
+  }
+
+
+
+}
